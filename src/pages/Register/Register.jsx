c26e7f676baa2e76c94853ee62500156060c7144
duclong7912 <duclong7912@gmail.com>
2022-12-13 15:09:50 +0700

    long update font

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div className="register">
+    <div className="register text-center">
       <div className="title">
         <h1>Register</h1>
       </div>
-      <form className='form__register'>
-        <div className="form__content">
+      <div className="register__wrapper">
+        <form className="form__register">
           <div className="form__input">
-            <input type="text" required/>
+            <input type="email" required/>
             <span>Email</span>
           </div>
           <div className="form__input">
@@ -17,32 +18,38 @@ const Register = () => {
             <span>Name</span>
           </div>
           <div className="form__input">
-            <input type="text" required/>
+            <input type="password" required/>
             <span>Password</span>
-            <i class="fa-solid fa-eye"></i>
           </div>
           <div className="form__input">
-            <input type="text" required/>
+            <input type="password" required/>
             <span>Comfirm password</span>
-            <i class="fa-solid fa-eye"></i>
           </div>
           <div className="form__input">
             <input type="text" required/>
             <span>Phone</span>
           </div>
-          <div className="form__gender text-left">
-            <span>Gender</span>
+          <div className="form__gender">
+            <span>Gender:</span>
             <label htmlFor="male">
-              <input type="radio" id='male' name='gender'/>
+              <input type="radio" id='male' name='gender' checked/>
+              <span className="radio"></span>
               <span>Male</span>
             </label>
             <label htmlFor="female">
-              <input type="radio" id='female' name='gender'/>
+              <input type="radio" id='female' name='gender' />
+              <span className="radio"></span>
               <span>Female</span>
             </label>
           </div>
+          <div className="form__submit-register">
+            <button className='btn-submit-register'>Submit</button>
+          </div>
+        </form>
+        <div className="form__info">
+        <span>Do you already have an account? <NavLink to={'/login'}>Login</NavLink></span>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
